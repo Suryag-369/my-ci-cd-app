79bcc8095f1d8ea50ad24833f0e641c43b763a83
Suryag-369 <suryanadiu369@gmail.com>
2025-07-16 16:53:56 +0530

    error : code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-indigo-600 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind + React + CI/CD 🌟</h1>
     </div>
   );
 }
